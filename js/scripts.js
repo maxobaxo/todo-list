8b32ce61218d8e0050c2bf03b65f4c4b6ed77f72
Max Scher and Rena Rallis <rena@>
2017-06-12 16:27:12 -0700

    implement object oriented programming into JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
 //business logic
+function Todo (task, description, deadline) {
+  this.todo = task;
+  this.descrip = description;
+  this.deadline = deadline;
+}
 
 //user interface logic
 $(document).ready(function() {
   $("#new-to-do").submit(function(event) {
     event.preventDefault();
 
-    var userInput = $("input#new-item").val();
-    $(".to-do-list").append("<li class='task'>" + userInput + "</li>");
-    
+    var todo = $("input#new-item").val();
+    var descrip = $("input#new-descrip").val();
+    var deadline = $("input#new-deadline").val();
+    var newTodo = new Todo (todo, descrip, deadline);
+
+    $(".to-do-list").append("<li class='task'>" + todo + "</li>");
+
     $(".task").click(function(event) {
       event.preventDefault();
 
       $(this).addClass("done");
     });
 
+    $(".task").last().hover(function(event) {
+      event.preventDefault();
+
+      $("#details").toggle();
+      $("h4").text(newTodo.todo);
+      $(".descrip").text(newTodo.descrip);
+      $(".deadline").text(newTodo.deadline);
+
+    });
     $(".results").show();
+
   });
 
   $("#checkables").submit(function(event) {
